3b2b0b77faf001ddb7a09008f3fb9bfd20a1cab3
zengyu <1213814232@qq.com>
2018-01-25 20:11:15 +0800

    update

--- demo.js
+++ demo.js
@@ -1,18 +1,23 @@
+var life = 0;
 var emotions = document.getElementsByName("emotion");
-var lifeItem = document.getElementById("life");
 
 function donateLife(){
-	alert("Your life:\t\t-1s\nElder's life:\t+1s\nThank you!");
-	lifeItem.value = parseInt(lifeItem.value) + 1;
-	console.e(lifeItem.value);
-	alert(lifeItem.value);
-	lifeItem.innerHTML="lifeItem.value";
+	alert("Your life: -1s\nElder's life: +1s\nThank you!");
+	document.getElementById("life").innerHTML = ++life;
 }
 
 function selectEmotion(){
 	for(i=0;i<emotions.length;i++){
 		if(emotions[i].checked){
-			alert("Your emotion is " + emotions[i].value);
+			var output;
+			if(i==0){
+				output = "给人一种钦定的感觉";
+			}else if(i==1){
+				output = "I'm angry!";
+			}else if(i==2){
+				output = "水可载舟，亦可赛艇";
+			}
+			alert(output);
 			break;
 		}
 	}
